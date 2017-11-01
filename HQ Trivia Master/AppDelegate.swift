b3e136a98b1d58b48d9dc39c63326f6e4afc0a80
Dani Smith <ds1239@hotmail.com>
2017-10-31 21:26:25 -0400

    Initial Commit

---
+++ HQ Trivia Master/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HQ Trivia Master
+//
+//  Created by Daniel Smith on 10/31/17.
+//  Copyright © 2017 Daniel Smith. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HQ Trivia Master/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HQ Trivia Master
+//
+//  Created by Daniel Smith on 10/31/17.
+//  Copyright © 2017 Daniel Smith. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
